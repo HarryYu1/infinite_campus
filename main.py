bc5207d9ee61cc3a3a6df144a01d82fd10cedc74
HarryYu1 <89357577+HarryYu1@users.noreply.github.com>
2023-04-09 18:34:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+#SQL test
+#from MySQLdb import _mysql
+import pymysql
+import pandas as pd
+import matplotlib.pyplot as plt
+
+connection = pymysql.connect(host = 'db.redhawks.us', user = 'ic_student', passwd = '1WillN0t$h@reThis!', database = 'naperville')
+
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute(
+            'SELECT * FROM `[203_NCHS_Progress_Grades]`'
+        )
+        print('connected!')
+
+        df = pd.DataFrame(cursor.fetchall(), columns = ['Index', 'IC_ID', 'CourseName', 'TeacherName', 'LetterGrade', 'Percentage'])
+    
+
+#students = df['IC_ID'].unique()
+
+#print(students)
+#print(students.size)
+
+#groups data in 
+min_value = df.groupby('IC_ID').Percentage.min()
+#merge
+df = df.merge(min_value, on='IC_ID',suffixes=('', '_min'))
+#drop B_min and also keep only data where B = min
+df = df[df.Percentage==df.Percentage_min].drop('Percentage_min', axis=1)
+
+print(df)
+
+#taking a peek at the grade distribution of everyone....
+plt.hist(df["Percentage"], bins = 100)
+plt.show()
+
+'''
+#grant all privileges on *.* to 'ic_student'@'%' with grant option;
+SELECT current_user();
+SELECT user();
+#GRANT ALL PRIVILEGES ON naperville.* TO 'ic_student'@'%';
+#FLUSH PRIVILEGES;
+
+CREATE USER 'ic_student'@"%" IDENTIFIED BY '1WillN0t$h@reThis!';
+GRANT ALL PRIVILEGES ON DATABASE.* TO 'ic_student'@"%" IDENTIFIED BY '1WillN0t$h@reThis!';
+
+SHOW GRANTS;'''
