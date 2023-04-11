f54ec8cb7ac6d2c79daa2799d7f7392c866c212f
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-04-11 13:58:54 -0500

    Remove Courses with a grade higher than 90

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ with connection.cursor() as cursor:
   cursor.execute('''
                 SELECT StudentID, CourseName 
                 FROM `[203_NCHS_Progress_Grades]` 
+                WHERE CoursePercent < 90
                 ORDER BY StudentID ASC, CoursePercent ASC
                 ''')
   print('connected!')
@@ -18,12 +19,19 @@ with connection.cursor() as cursor:
   # imports data into panda
   df = pd.DataFrame(cursor.fetchall(), columns = ['IC_ID', 'CourseName'])
 
-df = df.groupby('IC_ID')['CourseName'].apply(list)
+# makes list of courses for each student from lowest grade to
+# highest grade
+# idk how this works lol
+df = df.groupby('IC_ID')['CourseName'].apply(list).reset_index(name='Courses')
+print(df)
 
 # pseudocode for SOAR class assignment (WIP)
-# for (student class)
-
-print(df)
+# for (student in database)
+#   for (courses student is taking)
+#     if (SOAR for class isn't full)
+#       assign student to SOAR period
+#       return
+#   assign student to study hall
 
 # taking a peek at the grade distribution of everyone....
 #plt.hist(df["CourseName"], bins = 100)
