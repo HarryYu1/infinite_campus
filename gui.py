b41937e22289115456b7ee4cd44cc4051772a7c8
TheGrumpyMusician <100326980+Aternosus@users.noreply.github.com>
2023-04-13 20:57:12 -0500

    Update gui.py
    Just added some comments

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-def lock():
+def lock(): # Lockscreen
   window = Tk()
   root = Tk()
   window.title('Lock Screen')
@@ -13,30 +13,30 @@ def lock():
   lbl1.place(x=110, y=50)
   t1.place(x=200, y=50)
   root.withdraw()
-  lbl5 = Label(window, text = "Warning! Password Hardcoded.")
-  lbl5.place(x = 100, y = 150)
+  lbl5 = Label(window, text = "Warning! Password Hardcoded.") # Warning that should be removed when the issue is solved.
+  lbl5.place(x = 100, y = 150
   
-  def submit():
+  def submit(): 
     password = t1.get()
     secret = "123" #THIS IS A PASSWORD HARDCODED INTO THE PROGRAM! PLEASE REPLACE THIS WITH A .ENV KEY ASAP!
     
-    if password == secret:
+    if password == secret: # Gets rid of any text if password is correct
       lbl4 = Label(window, text = "")
       lbl4.place(x = 200, y = 100)
       passwordYes()
   
-    elif password == "":
+    elif password == "": # If password is empty
       lbl2 = Label(window, text = "Password Missing!")
       lbl2.place(x = 200, y = 100)
-    elif password != secret:
+    elif password != secret: # If password is wrong
       lbl3 = Label(window, text = "Wrong Password!")
       lbl3.place(x = 200, y = 100)
   
   def passwordYes():
-    window.destroy()
-    mainscreen()
+    window.destroy() # Destroys lockscreen when correct password is entered
+    mainscreen() # Opens up the mainscreen
     
-  b1 = Button(window, text='Enter', command=submit)
+  b1 = Button(window, text='Enter', command=submit) # Runs submit() function when pressed
   
   b1.place(x=390, y=46)
   
@@ -50,10 +50,10 @@ def mainscreen():
   window.geometry("600x400")
   root.withdraw()
   
-  def submit():
+  def runMain():
     import main # Seems sus but it works. If there are any future problems with main.py it might be because of this import statement. Just saying.
   
-  b1 = Button(window, text='Run!', command=submit)
+  b1 = Button(window, text='Run!', command=runMain) # Runs the import runMain() function
   
   b1.place(x=200, y=100)
   
