fc9d8da282ec13ca2b123cd6f06d9b9748ac077a
TheGrumpyMusician <100326980+Aternosus@users.noreply.github.com>
2023-04-17 11:08:46 -0500

    Update gui.py
    Updated to use Flask

--- gui.py
+++ gui.py
@@ -1,61 +1,30 @@
-from tkinter import *
+from flask import Flask, render_template, Response, request, redirect, url_for
 
-def lock(): # Lockscreen
-  window = Tk()
-  root = Tk()
-  window.title('Lock Screen')
-  window.geometry("600x400")
-  
-  lbl1 = Label(window, text="Password")
-  
-  t1 = Entry()
-  
-  lbl1.place(x=110, y=50)
-  t1.place(x=200, y=50)
-  root.withdraw()
-  lbl5 = Label(window, text = "Warning! Password Hardcoded.") # Warning that should be removed when the issue is solved.
-  lbl5.place(x = 100, y = 150
-  
-  def submit(): 
-    password = t1.get()
-    secret = "123" #THIS IS A PASSWORD HARDCODED INTO THE PROGRAM! PLEASE REPLACE THIS WITH A .ENV KEY ASAP!
-    
-    if password == secret: # Gets rid of any text if password is correct
-      lbl4 = Label(window, text = "")
-      lbl4.place(x = 200, y = 100)
-      passwordYes()
-  
-    elif password == "": # If password is empty
-      lbl2 = Label(window, text = "Password Missing!")
-      lbl2.place(x = 200, y = 100)
-    elif password != secret: # If password is wrong
-      lbl3 = Label(window, text = "Wrong Password!")
-      lbl3.place(x = 200, y = 100)
-  
-  def passwordYes():
-    window.destroy() # Destroys lockscreen when correct password is entered
-    mainscreen() # Opens up the mainscreen
-    
-  b1 = Button(window, text='Enter', command=submit) # Runs submit() function when pressed
-  
-  b1.place(x=390, y=46)
-  
-  window.mainloop()
+app = Flask(__name__)
 
-def mainscreen():
-  """A screen that just has a button to run the program."""
-  window = Tk()
-  root = Tk()
-  window.title('Main Window')
-  window.geometry("600x400")
-  root.withdraw()
+
+@app.route('/')
+def view_form():
+  return render_template('form.html')
   
-  def runMain():
-    import main # Seems sus but it works. If there are any future problems with main.py it might be because of this import statement. Just saying.
-  
-  b1 = Button(window, text='Run!', command=runMain) # Runs the import runMain() function
-  
-  b1.place(x=200, y=100)
-  
-  window.mainloop()
-lock()
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if username == "123" and password == "123": # HARDCODED PASSWORD! WARNING!!!!!!!!! >:(
+            return render_template('index.html')
+          
+        else:
+            return render_template('incorrect.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/initiateSOAR', methods=['POST'])
+def initiateSOAR():
+  import main # Imports the main.py file. 
+  return render_template('index.html')
+
+app.run(host='0.0.0.0', port=81)
+
