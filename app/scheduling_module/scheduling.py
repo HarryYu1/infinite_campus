0ddb78fcdf5d9781f5de6dffbd12bcc2a7706836
HarryYu1 <hyu01@stu.naperville203.org>
2023-04-27 19:37:31 -0500

    Quick Fix for access problem
    IMPORTANT: main was changed to find_lowest for readability

--- app/scheduling_module/scheduling.py
+++ app/scheduling_module/scheduling.py
@@ -9,12 +9,7 @@ schedulingbp = Blueprint('scheduling', __name__)
 @schedulingbp.route('/initiateSOAR', methods = ["POST"])
 def initiateSOAR():
   if request.method == 'POST':
-<<<<<<< Updated upstream
-    import main # Imports the main.py file. 
-    return render_template('scheduling/loading.html')
-=======
     from . import find_lowest # Imports find_lowest
     return render_template('scheduling/index.html')
->>>>>>> Stashed changes
   else:
     return redirect(url_for('auth.login'))
