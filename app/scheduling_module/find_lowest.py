b50a39c1732e438a2e34cb745fa672bf7a789322
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-06-04 16:26:15 -0500

    here you go Harry Yu man and Vinay
    bepis

--- app/scheduling_module/find_lowest.py
+++ app/scheduling_module/find_lowest.py
@@ -1,6 +1,13 @@
 import pymysql
 import pandas as pd
+import datetime
+from decimal import *
 import matplotlib.pyplot as plt
+
+def makeStudyHall(course):
+  while len(course) < 3:
+    course.append("Study Hall")
+  return course
 
 # connects to school sql database
 connection = pymysql.connect(host = 'db.redhawks.us', user = 'ic_student', passwd = '1WillN0t$h@reThis!', database = 'naperville')
@@ -9,21 +16,34 @@ with connection.cursor() as cursor:
   # fetches data from sql database
   # sorts each student's classes from lowest grade to highest
   cursor.execute('''
-                SELECT StudentID, CourseName 
-                FROM `[203_NCHS_Progress_Grades]` 
-                WHERE CoursePercent < 90
+                SELECT StudentID, CourseName, CoursePercent 
+                FROM `[203_NCHS_Progress_Grades]`
+                WHERE CoursePercent IS NOT NULL
                 ORDER BY StudentID ASC, CoursePercent ASC
                 ''')
   print('connected!')
 
   # imports data into panda
-  df = pd.DataFrame(cursor.fetchall(), columns = ['IC_ID', 'CourseName'])
+  df = pd.DataFrame(cursor.fetchall(), columns = ['StudentID', 'CourseName', 'CoursePercent'])
 
-# makes list of courses for each student from lowest grade to
-# highest grade
-# idk how this works lol
-df = df.groupby('IC_ID')['CourseName'].apply(list).reset_index(name='Courses')
+# takes the Coursename column and groups them by the student ID, then turns it into a list, then takes the 3 lowest grades from that list
+for i in df.index:
+  if df['CoursePercent'][i] >= 90: 
+    df['CourseName'][i] = 'Study Hall'
+
+df = df.groupby('StudentID')['CourseName'].apply(list).apply(lambda x : x[:3]).reset_index(name='CourseName').explode('CourseName').reset_index()
+
+df['Priority'] = df.groupby('index').cumcount() + 1
+
+df.drop(['index'], axis = 1, inplace = True)
+
+now = datetime.datetime(2009, 5, 5)
+
+
+
 print(df)
+
+
 
 # pseudocode for SOAR class assignment (WIP)
 # for (student in database)
